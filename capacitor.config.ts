bbbe7de1902137791563aefb6351f1cdd90897bb
ram6032 <123135317+ram6032@users.noreply.github.com>
2025-07-16 08:05:16 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.22dd3845edc14b7890a2cb0e16679c7e',
-  appName: 'app-creator-buddy-yes',
+  appId: 'com.ram.serviceconnect',
+  appName: 'Nestham',
   webDir: 'dist',
   server: {
     url: 'https://22dd3845-edc1-4b78-90a2-cb0e16679c7e.lovableproject.com?forceHideBadge=true',
