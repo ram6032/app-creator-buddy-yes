d85e09be4997f8a57921fc0397943d361ebb2b44
ram6032 <123135317+ram6032@users.noreply.github.com>
2025-07-16 08:26:29 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,16 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ram.serviceconnect',
-  appName: 'Nestham',
-  webDir: 'dist',
-  server: {
-    url: 'https://22dd3845-edc1-4b78-90a2-cb0e16679c7e.lovableproject.com?forceHideBadge=true',
-    cleartext: true
+{
+  "appId": "com.ram.serviceconnect",
+  "appName": "Nestham",
+  "webDir": "dist",
+  "server": {
+    "url": "https://22dd3845-edc1-4b78-90a2-cb0e16679c7e.lovableproject.com?forceHideBadge=true",
+    "cleartext": true
   },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#ffffff',
-      showSpinner: false
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 2000,
+      "backgroundColor": "#ffffff",
+      "showSpinner": false
     }
   }
-};
-
-export default config;
+}
